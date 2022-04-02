bbce388a5065c317dcd15a7060173cd11135237d
itspunitmehta <95695285+itspunitmehta@users.noreply.github.com>
2022-04-02 14:24:22 +0530

    settingError404inApp

--- app.js
+++ app.js
@@ -1,38 +1,18 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req,res)=>{
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-      res.write('<html>');
-      res.write('<head><title>Enter Message</title><head>');
-      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
-      res.write('</html>');
-      return res.end();
-    }
-    if(url==='/message' && method==='POST'){
-        const body = [];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.text', message);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<header><title>My Server Page</title></header>');
-    res.write('<body><h1> Welcome to my server</h1></body>');
-    res.write('</html>');
-    res.end();
+const app = express();
+
+const adminRoutes = require('./routers/admin');
+const shopRoutes = require('./routers/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h3>Page Not Found:404</h3>');
 })
 
-
-server.listen(3000);
+app.listen(3000);
